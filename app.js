cc3348f6de4f030ab4b25f70b09c003bdfdacc25
samuel21695 <samuel21695@gmail.com>
2023-10-10 13:09:21 +0900

    put each pokemon in li tag

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const pokemon = require('pokemon');
 const fs = require('fs');
-const data = `${pokemon.all('ko')}`;
-console.log(typeof(data))
+const data = pokemon.all('ko');
 
-fs.writeFile("pokemon.html", data, (err) => {
-  if (err) {
-    console.log('error has occured.');
-  } else {
-    console.log('file has created');
+const pokemonList = ""
+
+  for (i=0; i<data.length; i++) {
+    pokemonName = `<li>${data[i]}</li>`;
+    pokemonList += pokemonName
   }
-});
+
+
+console.log(pokemonList);
+
+// fs.writeFile("pokemon.html", data, (err) => {
+//   if (err) {
+//     console.log('error has occured.');
+//   } else {
+//     console.log('file has created');
+//   }
+// });
+
+
+
+// makeFile(data)
