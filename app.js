95663a7c080c622d0d04ff997bfc805183eae1f2
samuel21695 <samuel21695@gmail.com>
2023-10-10 14:52:13 +0900

    writeFile with pokemon li tag

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const pokemon = require('pokemon');
 const fs = require('fs');
 const data = pokemon.all('ko');
+let pokemonList = ""
 
-const pokemonList = ""
+for (i=0; i<data.length; i++) {
+  let pokemonName = `<li>${data[i]}</li>`;
+  pokemonList += pokemonName
+}
 
-  for (i=0; i<data.length; i++) {
-    pokemonName = `<li>${data[i]}</li>`;
-    pokemonList += pokemonName
+fs.writeFile("pokemon.html", pokemonList, (err) => {
+  if (err) {
+    console.log('error has occured.');
+  } else {
+    console.log('file has created');
   }
-
-
-console.log(pokemonList);
-
-// fs.writeFile("pokemon.html", data, (err) => {
-//   if (err) {
-//     console.log('error has occured.');
-//   } else {
-//     console.log('file has created');
-//   }
-// });
+});
 
 
 
